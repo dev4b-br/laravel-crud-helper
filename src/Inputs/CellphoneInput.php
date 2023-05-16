680e266da8372f78d6604731ae229bd0bc37f636
Gabriel Caminha <gabrielcaminha255@gmail.com>
2023-05-15 21:16:18 -0300

    add removeAreaCode method to cellphone input

--- src/Inputs/CellphoneInput.php
+++ src/Inputs/CellphoneInput.php
@@ -4,9 +4,15 @@ namespace Dev4b\LaravelCrudHelper\Inputs;
 
 class CellphoneInput extends TextInput
 {
-    protected array $maskBlocks = [0,2,0,5,4];
+    protected array $maskBlocks = [0, 2, 0, 5, 4];
 
-    protected ?array $maskDelimiter = ['(',')',' ','-'];
+    protected ?array $maskDelimiter = ['(', ')', ' ', '-'];
 
     protected bool $isNumericalOnly = true;
+
+    public function removeAreaCode()
+    {
+        $this->maskBlocks = array_diff($this->maskBlocks, [0, 2]);
+        $this->maskDelimiter = ['-'];
+    }
 }
