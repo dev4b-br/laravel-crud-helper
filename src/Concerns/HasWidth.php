9a0acd4c9563b044b6f858ea878ee09d3cd6e6ea
Gabriel Caminha <gabrielcaminha255@gmail.com>
2023-06-14 11:45:24 -0300

    add HasWidth trait

---
+++ src/Concerns/HasWidth.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Dev4b\LaravelCrudHelper\Concerns;
+
+trait HasWidth
+{
+    public function setHalf()
+    {
+        $this->addContainerClass('col-6');
+    }
+
+    public function setTriple()
+    {
+        $this->addContainerClass('col-4');
+    }
+}
