f632c8d72aa1942a30c61ef10577d5ae92e73a28
Gabriel Caminha <gabrielcaminha255@gmail.com>
2023-07-14 16:35:13 -0300

    implement specific password class

--- src/Inputs/PasswordInput.php
+++ src/Inputs/PasswordInput.php
@@ -2,7 +2,26 @@
 
 namespace Dev4b\LaravelCrudHelper\Inputs;
 
-class PasswordInput extends TextInput
+class PasswordInput extends AbstractInput
 {
     protected string $type = 'password';
+
+    protected bool $visibilityEye = true;
+
+    public function __construct(string $name, ?string $label = null, ?string $value = null, ?string $placeholder = null, ?string $id = null)
+    {
+        parent::__construct('laravel-crud-helper::inputs.password', $name, $label, $value, $placeholder, $id);
+    }
+
+    public function render()
+    {
+        $view = parent::render();
+        $view->with('type', $this->type);
+        return $view;
+    }
+
+    public function setVisibilityEye(bool $toggle)
+    {
+        $this->visibilityEye = $toggle;
+    }
 }
