354408aab5340c6f7d3020e1053c6eb4e54a8505
Gabriel Caminha <gabrielcaminha255@gmail.com>
2023-07-19 18:14:46 -0300

    implement old logic on textarea input

--- src/Inputs/TextAreaInput.php
+++ src/Inputs/TextAreaInput.php
@@ -2,16 +2,22 @@
 
 namespace Dev4b\LaravelCrudHelper\Inputs;
 
+use Dev4b\LaravelCrudHelper\Concerns\HasOld;
+
 class TextAreaInput extends AbstractInput
 {
+    use HasOld;
+
     public function __construct(string $name, ?string $label = null, ?string $value = null, ?string $placeholder = null, string $id = null)
     {
+        $this->oldKey = $name;
         parent::__construct('laravel-crud-helper::inputs.textarea', $name, $label, $value, $placeholder, $id);
     }
 
     public function render()
     {
         $view = parent::render();
+        $view->with('oldKey', $this->oldKey);
 
         return $view;
     }
