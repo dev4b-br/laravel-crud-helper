bb9f5dedff9af8a5d6375677fd186c60093e4a75
Gabriel Caminha <gabrielcaminha255@gmail.com>
2023-07-19 18:33:22 -0300

    implement old logic on money input

--- resources/views/inputs/money.blade.php
+++ resources/views/inputs/money.blade.php
@@ -2,7 +2,7 @@
     <label for="{{ $id }}" class="form-label">{{ $label }}</label>
     <input type="text" class="form-control
            @if($errors->get($name)) is-invalid @endif {{ implode(' ', $inputClasses) }}" name="{{ $name }}"
-           id="{{ $id }}" placeholder="{{ $placeholder }}" value="{{ $value }}"/>
+           id="{{ $id }}" placeholder="{{ $placeholder }}" value="{{ old($oldKey) ?? $value }}"/>
     @if($hint)
         <div class="form-text">{{ $hint }}</div>
     @endif
--- src/Inputs/MoneyInput.php
+++ src/Inputs/MoneyInput.php
@@ -2,19 +2,25 @@
 
 namespace Dev4b\LaravelCrudHelper\Inputs;
 
+use Dev4b\LaravelCrudHelper\Concerns\HasOld;
+
 class MoneyInput extends AbstractInput
 {
+    use HasOld;
+
     protected ?string $currency = 'R$';
 
     public function __construct(string $name, ?string $label = null, ?string $value = null, ?string $placeholder = null, ?string $id = null)
     {
+        $this->oldKey = $name;
         parent::__construct('laravel-crud-helper::inputs.money', $name, $label, $value, $placeholder, $id);
     }
 
     public function render()
     {
         $view = parent::render();
-        $view->with('currency', $this->currency);
+        $view->with('currency', $this->currency)
+            ->with('oldKey', $this->oldKey);
 
         return $view;
     }
