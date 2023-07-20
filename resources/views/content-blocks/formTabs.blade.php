a525a186facf3b685fd53ddcb347099f4522c825
Gabriel Caminha <gabrielcaminha255@gmail.com>
2023-07-19 21:10:09 -0300

    add formTabs example

---
+++ resources/views/content-blocks/formTabs.blade.php
@@ -0,0 +1,32 @@
+<div class="nav-align-top">
+    <ul class="nav nav-tabs" role="tablist">
+        <li class="nav-item">
+            <button type="button" class="nav-link active" role="tab" data-bs-toggle="tab"
+                    data-bs-target="#navs-top-home">First
+            </button>
+        </li>
+        <li class="nav-item">
+            <button type="button" class="nav-link" role="tab" data-bs-toggle="tab"
+                    data-bs-target="#tab-2">Second
+            </button>
+        </li>
+    </ul>
+    <div class="tab-content">
+        <div class="tab-pane fade show active" id="tab-1" role="tabpanel">
+            <div class="form-floating form-floating-outline">
+                <input type="text" id="firstExampleInput" class="form-control"/>
+                <label for="firstExampleInput">First Input</label>
+            </div>
+            <div class="form-floating form-floating-outline">
+                <input type="email" id="secondExampleInput" class="form-control"/>
+                <label for="secondExampleInput">Second Input</label>
+            </div>
+        </div>
+        <div class="tab-pane fade" id="tab-2" role="tabpanel">
+            <div class="form-floating form-floating-outline">
+                <input type="password" id="thirdExampleInput" class="form-control"/>
+                <label for="thirdExampleInput">Third Input</label>
+            </div>
+        </div>
+    </div>
+</div>
