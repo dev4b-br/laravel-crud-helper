1c8e2e242e0e2285dbe0202dc4431f624e96055f
Gabriel Caminha <gabrielcaminha255@gmail.com>
2023-07-27 20:10:43 -0300

    add remoteSelect view

---
+++ resources/views/inputs/remoteSelect.blade.php
@@ -0,0 +1,52 @@
+<div class="mb-3 {{ implode(' ', $containerClasses) }}" id="{{$id}}-container">
+    <label for="{{ $id }}" class="form-label">{{ $label }}</label>
+    <select @if($isMultiple) multiple
+            @endif class="form-select select2 @if($errors->get($name)) is-invalid @endif {{ implode(' ', $inputClasses) }}"
+            name="{{ $name }}@if($isMultiple)[]@endif" id="{{ $id }}">
+        @if($defaultOption)
+            <option value="">{{$defaultOption}}</option>
+        @endif
+    </select>
+    @if($hint)
+        <div class="form-text">{{ $hint }}</div>
+    @endif
+    @if($showInputErrorMessages)
+        @if($errors->get($name))
+            @foreach($errors->get($name) as $error)
+                <div class="invalid-feedback">
+                    {{$error}}
+                </div>
+            @endforeach
+        @endif
+    @endif
+</div>
+
+@section('laravel-crud-helper-scripts')
+    @parent
+    <script>
+        var CSRF_TOKEN = $('meta[name="csrf-token"]').attr('content');
+
+        $(document).ready(function () {
+            $("#{{ $id }}").select2({
+                language: "pt-BR",
+                minimumResultsForSearch: -1,
+                ajax: {
+                    type: "POST",
+                    url: "{{route($route)}}",
+                    dataType: "json",
+                    data: function (params) {
+                        return {
+                            _token: CSRF_TOKEN,
+                        };
+                    },
+                    processResults: function (response) {
+                        return {
+                            results: response
+                        };
+                    },
+                    cache: true,
+                }
+            });
+        })
+    </script>
+@endsection
