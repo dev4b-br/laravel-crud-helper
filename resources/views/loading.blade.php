477106818d250a47b77b65a807cc244d81e54002
Gabriel Caminha <gabrielcaminha255@gmail.com>
2023-08-07 16:21:34 -0300

    add modal-loading view

---
+++ resources/views/loading.blade.php
@@ -0,0 +1,16 @@
+<div class="modal modal-transparent fade" id="modal-loading" data-bs-backdrop="static">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-body d-flex justify-content-center">
+                <div class="sk-chase sk-primary" style="width: 60px; height: 60px">
+                    <div class="sk-chase-dot"></div>
+                    <div class="sk-chase-dot"></div>
+                    <div class="sk-chase-dot"></div>
+                    <div class="sk-chase-dot"></div>
+                    <div class="sk-chase-dot"></div>
+                    <div class="sk-chase-dot"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
