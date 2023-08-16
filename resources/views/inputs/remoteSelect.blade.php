d5431bac7398ccfa57590f4aa0dfb48a4e6c379d
Everton Muniz <munizeverton@gmail.com>
2023-08-15 23:14:58 -0300

    Fixes code style

--- resources/views/inputs/remoteSelect.blade.php
+++ resources/views/inputs/remoteSelect.blade.php
@@ -37,9 +37,8 @@
                     url: "{{$route}}",
                     dataType: 'json',
                     headers: {
-  'X-CSRF-TOKEN': '{!! csrf_token() !!}'
-},
-
+                        'X-CSRF-TOKEN': '{!! csrf_token() !!}'
+                    },
                     processResults: function (response) {
                         return {
                             results: response
