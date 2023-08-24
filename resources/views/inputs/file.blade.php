d142da1f9ece8a025398f4c89b29e7355abbbecc
Lucas Freitas <lucas@dev4b.com.br>
2023-08-24 17:24:51 -0300

    multiple conditional file input

--- resources/views/inputs/file.blade.php
+++ resources/views/inputs/file.blade.php
@@ -1,9 +1,10 @@
+
 <div class="mb-3 {{ implode(' ', $containerClasses) }}" id="{{$id}}-container">
     <label for="{{ $id }}" class="form-label">{{ $label }}@if($required)
             <span class="text-danger">*</span>
         @endif</label>
-    <input type="file" class="form-control @if($errors->get($name)) is-invalid @endif {{ implode(' ', $inputClasses) }}"
-           name="{{ $name }}" id="{{ $id }}" placeholder="{{ $placeholder }}"/>
+    <input type="file" @if($isMultiple) multiple @endif class="form-control @if($errors->get($name)) is-invalid @endif {{ implode(' ', $inputClasses) }}"
+           name="{{ $name }}@if($isMultiple)[]@endif" id="{{ $id }}" placeholder="{{ $placeholder }}"/>
     @if($hint)
         <div class="form-text">{{ $hint }}</div>
     @endif
