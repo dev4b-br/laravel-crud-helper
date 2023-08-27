1d4c714fbb0a0e2bc903fe2602157bf44296ae5c
Everton Muniz <munizeverton@gmail.com>
2023-08-27 09:37:53 -0300

    Adds new HasWidth generic method

--- src/Concerns/HasWidth.php
+++ src/Concerns/HasWidth.php
@@ -4,18 +4,38 @@ namespace Dev4b\LaravelCrudHelper\Concerns;
 
 trait HasWidth
 {
+    /**
+     * @return void
+     * @deprecated
+     * @see setColsNumber
+     */
     public function setHalf()
     {
         $this->addContainerClass('col-6');
     }
 
+    /**
+     * @return void
+     * @deprecated
+     * @see setColsNumber
+     */
     public function setTriple()
     {
         $this->addContainerClass('col-4');
     }
 
+    /**
+     * @return void
+     * @deprecated
+     * @see setColsNumber
+     */
     public function setQuadruple()
     {
         $this->addContainerClass('col-3');
+    }
+
+    public function setColsNumber($colsNumber)
+    {
+        $this->addContainerClass('col-' . $colsNumber);
     }
 }
