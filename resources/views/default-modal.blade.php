dadc20ff919959580867772f036e9fbc30db6927
kaique <kaiqueramoslisboasaldanha@gmail.com>
2024-02-16 14:10:34 -0300

    feat/ add modal button

---
+++ resources/views/default-modal.blade.php
@@ -0,0 +1,12 @@
+<div class="modal fade" id="defaultModal">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content" id="defaultModalContent">
+        </div>
+    </div>
+</div>
+
+<script>
+    function openDefaultModal() {
+        $('#defaultModal').modal('show');
+    }
+</script>
